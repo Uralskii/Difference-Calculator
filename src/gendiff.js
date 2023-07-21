429575e4e274e86422c5d04849d3c5a935a18298
Konstantin Kolmakov <uralskiics@gmail.com>
2023-07-21 10:54:49 +0500

    update gendiff

--- src/gendiff.js
+++ src/gendiff.js
@@ -4,8 +4,11 @@ import { compareAndSortedKeys, resultOfDiff } from '../formatters/plain.js';
 import parsedFile from '../parsers/parser.js';
 
 const gendiff = (path1, path2) => {
-  const file1 = fs.readFileSync(path.resolve(path1), 'utf-8');
-  const file2 = fs.readFileSync(path.resolve(path2), 'utf-8');
+  const data1 = path.resolve(process.cwd(), path1);
+  const data2 = path.resolve(process.cwd(), path2);
+
+  const file1 = fs.readFileSync(data1, 'utf-8');
+  const file2 = fs.readFileSync(data2, 'utf-8');
 
   const extFile1 = path.extname(path1);
   const extFile2 = path.extname(path2);
