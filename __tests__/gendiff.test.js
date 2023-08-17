1891f6f77d9d4fcd9cf4fab2dde795683b83ab2b
Konstantin Kolmakov <uralskiics@gmail.com>
2023-08-17 13:56:28 +0500

    fix tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,7 +1,7 @@
+import { fileURLToPath } from 'url';
 import { test, expect } from '@jest/globals';
 import fs from 'fs';
 import path from 'path';
-import { fileURLToPath } from 'url';
 import gendiff from '../src/gendiff.js';
 
 const __filename = fileURLToPath(import.meta.url);
