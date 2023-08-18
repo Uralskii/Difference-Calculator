8a7d6aa25d91a2a445df3b376487e39fedf4956b
Konstantin Kolmakov <uralskiics@gmail.com>
2023-08-18 10:07:03 +0500

    add tests formatters

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -11,9 +11,13 @@ test('gendiff', () => {
   const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
   const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-  const plain = readFile('file1_test.txt');
+  const stylish = readFile('file1_test.txt');
+  const plain = readFile('file2_test.txt');
+  const json = readFile('file3_test.json');
   const file1 = getFixturePath('file1.json');
   const file2 = getFixturePath('file2.json');
 
-  expect(gendiff(file1, file2)).toEqual(plain);
+  expect(gendiff(file1, file2, 'stylish')).toEqual(stylish);
+  expect(gendiff(file1, file2, 'plain')).toEqual(plain);
+  expect(gendiff(file1, file2, 'json')).toEqual(json);
 });
